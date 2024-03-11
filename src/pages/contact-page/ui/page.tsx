fb8cade9d5dc1156f35f37cc6867b43ca0b75f0a
ecomnazar <ecomnazar.me@gmail.com>
2024-03-11 11:12:59 +0500

    [added]: map with iframe

--- src/pages/contact-page/ui/page.tsx
+++ src/pages/contact-page/ui/page.tsx
@@ -13,15 +13,26 @@ export const ContactPage = () => {
                     <h3 className='font-bold text-[24px]'>{t('contacts')}</h3>
                     <h4 className='font-medium text-[22px] mb-4'>{t('technicalSupport')}</h4>
                     <ul className='space-y-2 font-medium'>
-                        <li><a href="tel:+99312753747">+993 (12) 75-37-47</a></li>
-                        <li><a href="tel:+99361859499">+993 (61) 85-94-99</a></li>
+                        <li><a href="mailto:sales@gozle.store">sales@gozle.store</a></li>
+
                     </ul>
                 </div>
                 <div>
                     <h4 className='font-medium text-[22px] mb-4'>{t('salesDepartment')}</h4>
                     <ul className='space-y-2 font-medium'>
                         <li><a href="mailto:sales@gozle.store">sales@gozle.store</a></li>
-                        <li><a href="tel:+99361859499">+993 (61) 85-94-99</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className='font-medium text-[22px] mb-4'>{t('forContact')}</h4>
+                    <ul className='space-y-2 font-medium'>
+                        <li><a href="mailto:sales@gozle.store">press@gozle.store</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className='font-medium text-[22px] mb-4'>{t('errorReports')}</h4>
+                    <ul className='space-y-2 font-medium'>
+                        <li><a href="mailto:sales@gozle.store">bugs@gozle.store</a></li>
                     </ul>
                 </div>
             </Container>
--- src/shared/map/index.tsx
+++ src/shared/map/index.tsx
@@ -1,6 +1,5 @@
 // import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
 
 export const Map = () => {
   // const markerIcon = new Icon({
@@ -9,14 +8,17 @@ export const Map = () => {
   // });
 
   return (
-    <MapContainer
-      attributionControl={false}
-      center={[37.900934, 58.404260]}
-      zoom={13}
-      style={{ width: "100%", height: "100%", borderRadius: "10px" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {/* <Marker icon={markerIcon} position={[37.969908, 58.320069]}></Marker> */}
-    </MapContainer>
+    <>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1nypOeOH5banj3IKb2PmZEv5RXz2aCMQ&ehbc=2E312F" className="w-full h-[600px]"></iframe>
+    </>
+    // <MapContainer
+    //   attributionControl={false}
+    //   center={[37.900934, 58.404260]}
+    //   zoom={13}
+    //   style={{ width: "100%", height: "100%", borderRadius: "10px" }}
+    // >
+    //   <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    //   {/* <Marker icon={markerIcon} position={[37.969908, 58.320069]}></Marker> */}
+    // </MapContainer>
   );
 };
